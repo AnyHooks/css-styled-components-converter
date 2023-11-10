66b26b784da2284e68c85dc3381ab28e1e1026ef
Malnati <ricardomalnati@gmail.com>
2023-11-10 22:15:13 +0100

    REFACTORING(core 🧱) Refactored `loadCSSFromHTML` to `loadCSSFromURL` - Refactored the implementation to load CSS from a given URL instead of HTML and added error handling. Also renamed `loadAndParseCSS` for clarity and made it async. Updated `main` function.
    Refactored the `loadCSSFromHTML` function to `loadCSSFromURL` and modified the implementation to load CSS from a given URL instead of HTML. Also added error handling for loading CSS from the URL. Renamed the `loadAndParseCSS` function for clarity and made it async. Updated the `main` function to use the new function names and handle loading and parsing CSS from both URLs and local files.

--- cssFromURL.js
+++ cssFromURL.js
@@ -4,69 +4,43 @@ const cssSelect = require('css-select');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function loadCSSFromHTML(url) {
-  try {
-    const htmlResponse = await axios.get(url);
-    const $ = cheerio.load(htmlResponse.data);
-    const cssLinks = [];
-
-    $('link[rel="stylesheet"]').each((index, element) => {
-      cssLinks.push($(element).attr('href'));
-    });
-
-    return Promise.all(cssLinks.map(async link => {
-      try {
-        const response = await axios.get(link);
+async function loadCSSFromURL(url) {
+    try {
+        const response = await axios.get(url);
         return response.data;
-      } catch (error) {
-        console.error(`Erro ao carregar CSS de ${link}: ${error.message}`);
-        return '';  // Retorna uma string vazia em caso de erro
-      }
-    }));
-  } catch (error) {
-    console.error(`Erro ao carregar HTML: ${error.message}`);
-    return []; // Retorna um array vazio em caso de erro
-  }
+    } catch (error) {
+        console.error(`Erro ao carregar CSS de ${url}: ${error.message}`);
+        return '';
+    }
 }
 
-async function loadCSSFromHTML(url) {
-  try {
-    const htmlResponse = await axios.get(url);
-    const $ = cheerio.load(htmlResponse.data);
-    const cssLinks = [];
+async function loadAndParseCSS(url) {
+    const cssData = await loadCSSFromURL(url);
+    return css.parse(cssData);
+}
 
-    $('link[rel="stylesheet"]').each((index, element) => {
-      cssLinks.push($(element).attr('href'));
+async function processCSSContent(parsedCSS, selectors) {
+    selectors.forEach(selector => {
+        let validSelector = selector.includes('class="') ? selector.replace('class="', '.').replace('"', '') :
+                            selector.includes('id="') ? selector.replace('id="', '#').replace('"', '') : selector;
+        const result = cssSelect.selectOne(validSelector, parsedCSS.stylesheet.rules);
+        console.log(result ? `Found: '${validSelector}'` : `Not found: '${validSelector}'`);
     });
-
-    return Promise.all(cssLinks.map(link => axios.get(link).then(response => response.data)));
-  } catch (error) {
-    console.error(`Erro ao carregar CSS: ${error.message}`);
-  }
-}
-
-async function processCSSContent(cssContent, selectors) {
-  const parsedStyles = css.parse(cssContent);
-  selectors.forEach(selector => {
-    let validSelector = selector.includes('class="') ? selector.replace('class="', '.').replace('"', '') :
-                        selector.includes('id="') ? selector.replace('id="', '#').replace('"', '') : selector;
-    const result = cssSelect.selectOne(validSelector, parsedStyles.stylesheet.rules);
-    console.log(result ? `Found: '${validSelector}'` : `Not found: '${validSelector}'`);
-  });
 }
 
 async function main() {
-  const selectors = fs.readFileSync(process.argv[2], 'utf8').split('\n');
+    const selectors = fs.readFileSync(process.argv[2], 'utf8').split('\n');
 
-  for (let i = 3; i < process.argv.length; i++) {
-    if (process.argv[i].startsWith('http://') || process.argv[i].startsWith('https://')) {
-      const cssContents = await loadCSSFromHTML(process.argv[i]);
-      cssContents.forEach(cssContent => processCSSContent(cssContent, selectors));
-    } else {
-      const cssContent = fs.readFileSync(process.argv[i], 'utf8');
-      processCSSContent(cssContent, selectors);
+    for (let i = 3; i < process.argv.length; i++) {
+        if (process.argv[i].startsWith('http://') || process.argv[i].startsWith('https://')) {
+            const parsedCSS = await loadAndParseCSS(process.argv[i]);
+            await processCSSContent(parsedCSS, selectors);
+        } else {
+            const cssContent = fs.readFileSync(process.argv[i], 'utf8');
+            const parsedCSS = css.parse(cssContent);
+            await processCSSContent(parsedCSS, selectors);
+        }
     }
-  }
 }
 
 main();
