5618d10ee29ecdb0a66df2264591556c9ac245c3
Malnati <ricardomalnati@gmail.com>
2023-11-10 21:37:48 +0100

    MAJOR(core 🧱): Refactored code to process multiple CSS files - The code was refactored to process multiple CSS files instead of just one, improving the flexibility and scalability of the program.
    Refactored the code to process multiple CSS files instead of just one. Each CSS file is now read and parsed separately. Iterated through the CSS files passed as command-line arguments to process each one. Also, added error handling to catch and display any errors that occur during the processing of selectors. Updated the console output to include the name of the CSS file where each selector was found or not found.

--- cssSelect.js
+++ cssSelect.js
@@ -2,46 +2,33 @@ const fs = require('fs');
 const css = require('css');
 const cssSelect = require('css-select');
 
-// Read and parse the CSS file
-const cssContent = fs.readFileSync(process.argv[3], 'utf8');
-const parsedStyles = css.parse(cssContent);
-
 // Read the text file with selectors
 const selectors = fs.readFileSync(process.argv[2], 'utf8').split('\n');
 
+// Process each CSS file
+for (let i = 3; i < process.argv.length; i++) {
+    const cssContent = fs.readFileSync(process.argv[i], 'utf8');
+    const parsedStyles = css.parse(cssContent);
 
-selectors.forEach(selector => {
-    let validSelector;
-    try {
+    selectors.forEach(selector => {
+        let validSelector;
+        try {
+            if (selector.includes('class="')) {
+                validSelector = selector.replace('class="', '.').replace('"', '');
+            } else if (selector.includes('id="')) {
+                validSelector = selector.replace('id="', '#').replace('"', '');
+            } else {
+                validSelector = selector;
+            }
 
-        if (selector.includes('class="')) {
-            validSelector = selector.replace('class="', '.').replace('"', '');
-        } else if (selector.includes('id="')) {
-            validSelector = selector.replace('id="', '#').replace('"', '');
-        } else {
-            validSelector = selector;
+            const result = cssSelect.selectOne(validSelector, parsedStyles.stylesheet.rules);
+            if (result) {
+                console.log(`Found in ${process.argv[i]}: '${validSelector}'`);
+            } else {
+                console.log(`Not found in ${process.argv[i]}: '${validSelector}'`);
+            }
+        } catch (error) {
+            console.error(`Error processing selector '${validSelector}' in ${process.argv[i]}: ${error.message}`);
         }
-
-        const result = cssSelect.selectOne(validSelector, parsedStyles.stylesheet.rules);
-        if (result) {
-            console.log(`Found: '${validSelector}'`);
-        } else {
-            console.log(`Not found: '${validSelector}'`);
-        }
-    } catch (error) {
-        console.error(`Error processing selector '${validSelector}': ${error.message}`);
-    }
-});
-
-// Additional CSS file if needed
-const additionalCssContent = fs.readFileSync(process.argv[4], 'utf8');
-const additionalParsedStyles = css.parse(additionalCssContent);
-
-// Repeat the process for the additional CSS file
-selectors.forEach(selector => {
-    const result = cssSelect.selectOne(selector, additionalParsedStyles.stylesheet.rules);
-    if (result) {
-        console.log(`Found in additional CSS: '${selector}'`);
-    }
-});
-
+    });
+}
