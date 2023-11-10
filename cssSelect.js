865f044baaa18da1eabdd3a6068adab2cd3ae58f
Malnati <ricardomalnati@gmail.com>
2023-11-10 21:34:44 +0100

    Deleted selectors: MINOR(core 🔍): Removed unnecessary CSS selectors - '.active', '.ativo', '.botao__gera_paginacao_completa', '.breadcrumb', '.checkbox', '.close', '.collapse', '.container', '.dataTables_length', '.dataTables_paginate.paging_simple', '.even', '.gaveta', '.gaveta__corpo', '.gaveta__topo', '.input100', '.linkRendered', '.logo', '.modal.fade', '.odd', '.paginate_button.next', '.paginate_button.previous.disabled', '.pagination', '.passos.ativo', '.row.collapse.out.in', '.row', '', 'id="_hjSafeContext_25131878"', 'id="aconteudo"', 'id="alerts"', 'id="ano"', 'id="arodape"', 'id="baixarForm"', 'id="baixarModal"', 'id="botaoBuscar"', 'id="botaoMenu"', 'id="btnAgregarAdicionar"', 'id="btnBaixar"', 'id="btnCopiarLink"', 'id="btnDuvidas"', 'id="btnFecharBox"', 'id="btnGerarGrafico"', 'id="btnGerarLink"', 'id="btnMenuFlutNotific"', 'id="carregando"', 'id="cpfNisBeneficiario"', 'id="datas"', 'id="divButtonHeader"', 'id="divCompartilhar"', 'id="divQRCode"', 'id="duvidas"', 'id="header"', 'id="idFiltroBotao"', 'id="lista"', 'id="lista_length"', 'id="lista_next"', 'id="lista_paginate"', 'id="lista_previous"', 'id="lista_wrapper"', 'id="marcarDesmarcarAgregarAdicionar"', 'id="municipio"', 'id="nomeBeneficiario"', 'id="nomeMunicipio"', 'id="palavraChave"', 'id="spinner"', 'id="termoBuscaPortal"', 'id="tipoBeneficio"', 'id="tituloGrafico"', 'id="uf"', 'id="valorAte"', 'id="valorDe"'
    Deleted selectors:
    - '.active'
    - '.ativo'
    - '.botao__gera_paginacao_completa'
    - '.breadcrumb'
    - '.checkbox'
    - '.close'
    - '.collapse'
    - '.container'
    - '.dataTables_length'
    - '.dataTables_paginate.paging_simple'
    - '.even'
    - '.gaveta'
    - '.gaveta__corpo'
    - '.gaveta__topo'
    - '.input100'
    - '.linkRendered'
    - '.logo'
    - '.modal.fade'
    - '.odd'
    - '.paginate_button.next'
    - '.paginate_button.previous.disabled'
    - '.pagination'
    - '.passos.ativo'
    - '.row.collapse.out.in'
    - '.row'
    - ''
    - 'id="_hjSafeContext_25131878"'
    - 'id="aconteudo"'
    - 'id="alerts"'
    - 'id="ano"'
    - 'id="arodape"'
    - 'id="baixarForm"'
    - 'id="baixarModal"'
    - 'id="botaoBuscar"'
    - 'id="botaoMenu"'
    - 'id="btnAgregarAdicionar"'
    - 'id="btnBaixar"'
    - 'id="btnCopiarLink"'
    - 'id="btnDuvidas"'
    - 'id="btnFecharBox"'
    - 'id="btnGerarGrafico"'
    - 'id="btnGerarLink"'
    - 'id="btnMenuFlutNotific"'
    - 'id="carregando"'
    - 'id="cpfNisBeneficiario"'
    - 'id="datas"'
    - 'id="divButtonHeader"'
    - 'id="divCompartilhar"'
    - 'id="divQRCode"'
    - 'id="duvidas"'
    - 'id="header"'
    - 'id="idFiltroBotao"'
    - 'id="lista"'
    - 'id="lista_length"'
    - 'id="lista_next"'
    - 'id="lista_paginate"'
    - 'id="lista_previous"'
    - 'id="lista_wrapper"'
    - 'id="marcarDesmarcarAgregarAdicionar"'
    - 'id="municipio"'
    - 'id="nomeBeneficiario"'
    - 'id="nomeMunicipio"'
    - 'id="palavraChave"'
    - 'id="spinner"'
    - 'id="termoBuscaPortal"'
    - 'id="tipoBeneficio"'
    - 'id="tituloGrafico"'
    - 'id="uf"'
    - 'id="valorAte"'
    - 'id="valorDe"'

--- cssSelect.js
+++ cssSelect.js
@@ -9,22 +9,29 @@ const parsedStyles = css.parse(cssContent);
 // Read the text file with selectors
 const selectors = fs.readFileSync(process.argv[2], 'utf8').split('\n');
 
-// Process each selector
+
 selectors.forEach(selector => {
-    // Convert the string into a valid CSS selector
-    const validSelector = selector.replace('class="', '.').replace('"', '');
-  
+    let validSelector;
     try {
-      const result = cssSelect.selectOne(validSelector, parsedStyles.stylesheet.rules);
-      if (result) {
-        console.log(`Found: '${validSelector}'`);
-      } else {
-        console.log(`Not found: '${validSelector}'`);
-      }
+
+        if (selector.includes('class="')) {
+            validSelector = selector.replace('class="', '.').replace('"', '');
+        } else if (selector.includes('id="')) {
+            validSelector = selector.replace('id="', '#').replace('"', '');
+        } else {
+            validSelector = selector;
+        }
+
+        const result = cssSelect.selectOne(validSelector, parsedStyles.stylesheet.rules);
+        if (result) {
+            console.log(`Found: '${validSelector}'`);
+        } else {
+            console.log(`Not found: '${validSelector}'`);
+        }
     } catch (error) {
-      console.error(`Error processing selector '${validSelector}': ${error.message}`);
+        console.error(`Error processing selector '${validSelector}': ${error.message}`);
     }
-  });
+});
 
 // Additional CSS file if needed
 const additionalCssContent = fs.readFileSync(process.argv[4], 'utf8');
@@ -32,9 +39,9 @@ const additionalParsedStyles = css.parse(additionalCssContent);
 
 // Repeat the process for the additional CSS file
 selectors.forEach(selector => {
-  const result = cssSelect.selectOne(selector, additionalParsedStyles.stylesheet.rules);
-  if (result) {
-    console.log(`Found in additional CSS: '${selector}'`);
-  }
+    const result = cssSelect.selectOne(selector, additionalParsedStyles.stylesheet.rules);
+    if (result) {
+        console.log(`Found in additional CSS: '${selector}'`);
+    }
 });
 
