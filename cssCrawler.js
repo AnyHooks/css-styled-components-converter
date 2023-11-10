e35b92a0fadd4ec19eabb58ff133354bb655a575
Malnati <ricardomalnati@gmail.com>
2023-11-10 22:36:12 +0100

    MINOR (scripts ✨): Update css-crawler script and package.json
    The commit message for the given git diff output could be:
    
    "Update css-crawler script and package.json"
    
    This commit includes two changes:
    1. In the `cssCrawler.js` file, the change is to launch Puppeteer with `{ headless: "new" }` instead of just launching it without any arguments.
    2. In the `package.json` file, the change is to update the value of the `"css-crawler"` script to `node cssCrawler.js` instead of just `cssCrawler.js`.
    
    These changes improve the functionality and ensure the correct execution of the CSS crawler script.

--- cssCrawler.js
+++ cssCrawler.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function extractCSS(url) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
   await page.goto(url);
 
