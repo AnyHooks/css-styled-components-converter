1e1b4fbd16380c46b3306ac0e69b4e0f64561ed9
Malnati <ricardomalnati@gmail.com>
2023-11-10 21:46:57 +0100

    REFACTOR(MINOR 💻): Separate CSS loading and processing - Split code into two functions: loadCSSFromHTML and processCSSContent, use Promise.all for asynchronous fetching and processing, handle both local file paths and URLs in the main function.
    Refactor the code to load and process CSS from HTML and separate it into two functions: `loadCSSFromHTML` and `processCSSContent`. Instead of immediately processing each CSS file, gather all the CSS links from the HTML and use Promise.all to fetch and process them asynchronously. This improves efficiency and allows for better error handling. Additionally, modify the main function to handle both local file paths and URLs as arguments, allowing for greater flexibility in CSS processing.

--- cssSelect.js
+++ cssSelect.js
@@ -6,54 +6,42 @@ const cheerio = require('cheerio');
 
 async function loadCSSFromHTML(url) {
   try {
-    // Carrega o conteúdo HTML da URL
     const htmlResponse = await axios.get(url);
     const $ = cheerio.load(htmlResponse.data);
+    const cssLinks = [];
 
-    // Procura por todos os links de arquivos CSS
-    $('link[rel="stylesheet"]').each(async (index, element) => {
-      const cssLink = $(element).attr('href');
-
-      // Carrega cada arquivo CSS
-      const cssResponse = await axios.get(cssLink);
-      // Aqui você pode processar o conteúdo CSS conforme necessário
-      console.log(cssResponse.data);
+    $('link[rel="stylesheet"]').each((index, element) => {
+      cssLinks.push($(element).attr('href'));
     });
+
+    return Promise.all(cssLinks.map(link => axios.get(link).then(response => response.data)));
   } catch (error) {
     console.error(`Erro ao carregar CSS: ${error.message}`);
   }
 }
 
-// Substitua pela URL do seu HTML
-loadCSSFromHTML('https://example.com');
-
-// Read the text file with selectors
-const selectors = fs.readFileSync(process.argv[2], 'utf8').split('\n');
-
-// Process each CSS file
-for (let i = 3; i < process.argv.length; i++) {
-    const cssContent = fs.readFileSync(process.argv[i], 'utf8');
-    const parsedStyles = css.parse(cssContent);
-
-    selectors.forEach(selector => {
-        let validSelector;
-        try {
-            if (selector.includes('class="')) {
-                validSelector = selector.replace('class="', '.').replace('"', '');
-            } else if (selector.includes('id="')) {
-                validSelector = selector.replace('id="', '#').replace('"', '');
-            } else {
-                validSelector = selector;
-            }
-
-            const result = cssSelect.selectOne(validSelector, parsedStyles.stylesheet.rules);
-            if (result) {
-                console.log(`Found in ${process.argv[i]}: '${validSelector}'`);
-            } else {
-                console.log(`Not found in ${process.argv[i]}: '${validSelector}'`);
-            }
-        } catch (error) {
-            console.error(`Error processing selector '${validSelector}' in ${process.argv[i]}: ${error.message}`);
-        }
-    });
+async function processCSSContent(cssContent, selectors) {
+  const parsedStyles = css.parse(cssContent);
+  selectors.forEach(selector => {
+    let validSelector = selector.includes('class="') ? selector.replace('class="', '.').replace('"', '') :
+                        selector.includes('id="') ? selector.replace('id="', '#').replace('"', '') : selector;
+    const result = cssSelect.selectOne(validSelector, parsedStyles.stylesheet.rules);
+    console.log(result ? `Found: '${validSelector}'` : `Not found: '${validSelector}'`);
+  });
 }
+
+async function main() {
+  const selectors = fs.readFileSync(process.argv[2], 'utf8').split('\n');
+
+  for (let i = 3; i < process.argv.length; i++) {
+    if (process.argv[i].startsWith('http://') || process.argv[i].startsWith('https://')) {
+      const cssContents = await loadCSSFromHTML(process.argv[i]);
+      cssContents.forEach(cssContent => processCSSContent(cssContent, selectors));
+    } else {
+      const cssContent = fs.readFileSync(process.argv[i], 'utf8');
+      processCSSContent(cssContent, selectors);
+    }
+  }
+}
+
+main();
