1ee9941cd7003126c2b4aee857a0713aa8b981a6
Malnati <ricardomalnati@gmail.com>
2023-11-10 15:18:26 +0100

    REFACTOR(MINOR 🎨): Refactor code to read and parse CSS files
    Refactor code to read and parse CSS files, process selectors, and print results
    
    - Changed file import for `fs` module and `css` and `css-select` packages to use `require` statement
    - Added code to read the CSS file and parse its content using `fs.readFileSync` and `css.parse` functions
    - Replaced hardcoded selector with a variable `selectors` that is read from a text file using `fs.readFileSync` and `split` method to create an array of selectors
    - Iterated over each selector in `selectors` array and used `css-select.selectOne` function to find matches in the parsed CSS file
    - Added conditional statements to print whether a selector was found or not
    - Added code to read an additional CSS file and repeat the process for it
    - Printed the extracted styles for demonstration purposes

--- cssSelect.js
+++ cssSelect.js
@@ -1,11 +1,33 @@
+const fs = require('fs');
 const css = require('css');
 const cssSelect = require('css-select');
 
-// Parse the CSS file
-const parsedStyles = css.parse(yourCssFileContent);
+// Read and parse the CSS file
+const cssContent = fs.readFileSync(process.argv[3], 'utf8');
+const parsedStyles = css.parse(cssContent);
 
-// Extract styles using a selector
-const extractedStyles = cssSelect.selectOne('.your-selector', parsedStyles.stylesheet.rules);
+// Read the text file with selectors
+const selectors = fs.readFileSync(process.argv[2], 'utf8').split('\n');
 
-// Work with the extracted styles
-console.log(extractedStyles);
+// Process each selector
+selectors.forEach(selector => {
+  const result = cssSelect.selectOne(selector, parsedStyles.stylesheet.rules);
+  if (result) {
+    console.log(`Found: '${selector}'`);
+  } else {
+    console.log(`Not found: '${selector}'`);
+  }
+});
+
+// Additional CSS file if needed
+const additionalCssContent = fs.readFileSync(process.argv[4], 'utf8');
+const additionalParsedStyles = css.parse(additionalCssContent);
+
+// Repeat the process for the additional CSS file
+selectors.forEach(selector => {
+  const result = cssSelect.selectOne(selector, additionalParsedStyles.stylesheet.rules);
+  if (result) {
+    console.log(`Found in additional CSS: '${selector}'`);
+  }
+});
+
