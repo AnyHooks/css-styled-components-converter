8b3b23244cefb60c072cc6ac029f04d1ac084a13
Malnati <ricardomalnati@gmail.com>
2023-11-10 21:46:44 +0100

    feat(core ✨): Add css-select.txt, modify cssSelect.js & package.json
    This commit includes adding a new file `css-select.txt` with CSS selectors that need to be found in bootstrap.min.css but are not present. It also modifies `cssSelect.js` to include the use of `axios` and `cheerio` libraries to load CSS files from an HTML document and process their contents. Additionally, `package.json` is updated to include dependencies on `axios` and `cheerio`.
    
    Added new file `css-select.txt` with some CSS selectors that need to be found in bootstrap.min.css but are not present.
    Modified `cssSelect.js` to include the use of `axios` and `cheerio` libraries to load CSS files from an HTML document and process their contents.
    Updated `package.json` to include dependencies on `axios` and `cheerio`.

--- cssSelect.js
+++ cssSelect.js
@@ -1,6 +1,31 @@
 const fs = require('fs');
 const css = require('css');
 const cssSelect = require('css-select');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function loadCSSFromHTML(url) {
+  try {
+    // Carrega o conteúdo HTML da URL
+    const htmlResponse = await axios.get(url);
+    const $ = cheerio.load(htmlResponse.data);
+
+    // Procura por todos os links de arquivos CSS
+    $('link[rel="stylesheet"]').each(async (index, element) => {
+      const cssLink = $(element).attr('href');
+
+      // Carrega cada arquivo CSS
+      const cssResponse = await axios.get(cssLink);
+      // Aqui você pode processar o conteúdo CSS conforme necessário
+      console.log(cssResponse.data);
+    });
+  } catch (error) {
+    console.error(`Erro ao carregar CSS: ${error.message}`);
+  }
+}
+
+// Substitua pela URL do seu HTML
+loadCSSFromHTML('https://example.com');
 
 // Read the text file with selectors
 const selectors = fs.readFileSync(process.argv[2], 'utf8').split('\n');
